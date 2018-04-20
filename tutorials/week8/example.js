c38464aea4078c45cc8e5afcb9661892ddc0d7e2
Ervin Chua <ervinchua@gmail.com>
2018-04-20 21:00:57 +1000

    localStorage example

---
+++ tutorials/week8/example.js
@@ -0,0 +1,5 @@
+//
+
+// Regular expressions
+
+// Web storage
--- tutorials/week8/script.js
+++ tutorials/week8/script.js
@@ -1,5 +1,43 @@
-//
+if (localStorage) {
+  window.onload = populateFields;
+}
 
-// Regular expressions
+var remember = document.getElementById('remember')
+var submit = document.getElementById('submit')
 
-// Web storage
+remember.addEventListener('change', function() {
+  if (this.checked) {
+    setStorage()
+  } else {
+    localStorage.clear()
+  }
+})
+
+submit.addEventListener('click', function(e) {
+  document.getElementById('secret').innerHTML = "Woooooooo, now you can use this in your assignment!"
+  e.preventDefault()
+})
+
+function setStorage() {
+  var name = document.getElementById('name').value
+  var email = document.getElementById('email').value
+  var checkbox = document.getElementById('remember')
+  
+  localStorage.setItem('name', name)
+  localStorage.setItem('email', email)
+
+  if (checkbox.checked) {
+    localStorage.setItem('remember', true)
+  }
+}
+
+function populateFields() {
+  document.getElementById('name').value = localStorage.getItem('name')
+  document.getElementById('email').value = localStorage.getItem('email')
+
+  var checked = JSON.parse(localStorage.getItem('remember'))
+
+  if (checked == true) {
+    document.getElementById('remember').checked = checked;
+  }
+}
