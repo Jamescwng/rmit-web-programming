2d0285289571f0c6419f5a849e8b206eb2f6c537
Ervin Chua <ervinchua@gmail.com>
2018-04-20 21:29:56 +1000

    Eradicate all ES6 examples *heartbreak*

--- tutorials/week7/script.js
+++ tutorials/week7/script.js
@@ -6,22 +6,22 @@ document.getElementById("click");
 // These are the same as above
 document.querySelectorAll("p");
 document.querySelectorAll(".price");
-const button = document.querySelector("#click");
+var button = document.querySelector("#click");
 
 
 
 // Chaining event listeners on selectors
-let counter = 0;
+var counter = 1;
 button.addEventListener("click", function() {
-    counter++;
     console.log("You clicked me " + counter + " times.");
+    counter++;
 });
 
 
 
 // Number instance methods
 // MDN docs - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Number
-const number = 15989;
+var number = 15989;
 
 // Format number to two decimals
 console.log(number.toFixed(2));
@@ -36,10 +36,10 @@ console.log(number.toLocaleString("jp-JP", { style: "currency", currency: "JPY" 
 // Array instance methods and higher order functions
 // MDN docs - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array
 // Array explorer by Sarah Drasner - https://sdras.github.io/array-explorer/
-const tutors = ["Ervin", "Byron", "Trevor", "Adrian", "Edward", "Hai"];
+var tutors = ["Ervin", "Byron", "Trevor", "Adrian", "Edward", "Hai"];
 
 // Using for loop
-for (let i = 0; i < tutors.length; i++) {
+for (var i = 0; i < tutors.length; i++) {
     console.log(tutors[i] + " is my web programming tutor.");
 }
 
@@ -48,22 +48,17 @@ tutors.forEach(function(tutor) {
     console.log(tutor + " is my web programming tutor.");
 });
 
-// Using ES6 arrow functions with cheeky template literals
-tutors.forEach(tutor => {
-    console.log(`${tutor} is my web programming tutor.`);
-});
-
 
 
 // Template literals
 // MDN docs - https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
 
-const fName = "Ervin";
-const lName = "Chua";
-const age = 15;
+var fName = "Ervin";
+var lName = "Chua";
+var age = 15;
 
-const rubbish = "My name is " + fName + " " + lName + " and I'm " + age + " years old.";
-const better = `My name is ${fName} ${lName} and I'm ${age} years old.`;
+var rubbish = "My name is " + fName + " " + lName + " and I'm " + age + " years old.";
+var better = `My name is ${fName} ${lName} and I'm ${age} years old.`;
 
 console.log(rubbish);
 console.log(better);
