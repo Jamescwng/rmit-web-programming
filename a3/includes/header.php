3b06675ae5bf13faf21cd02e73355d9b75c3443c
Ervin Chua <ervinchua@gmail.com>
2018-04-21 12:01:55 +1000

    Fix route typo

--- a3/includes/header.php
+++ a3/includes/header.php
@@ -1,7 +1,7 @@
 <header>
   <nav>
     <ul>
-      <li><a href="home.php">Home</a></li>
+      <li><a href="index.php">Home</a></li>
       <li><a href="rates.php">Rates</a></li>
       <li><a href="accommodation.php">Accommodation</a></li>
       <li><a href="contact.php">Contact</a></li>
