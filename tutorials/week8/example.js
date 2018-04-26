eec53299d3f4a65c0eb54897c8f370c60b0fc361
Ervin Chua <ervinchua@gmail.com>
2018-04-26 11:42:14 +1000

    Examples for Objects and regular expressions

--- tutorials/week8/example.js
+++ tutorials/week8/example.js
@@ -1,5 +1,38 @@
-//
+// Objects
+var tutor = {
+  "name": "Ervin",
+  "school": "Science",
+  "courses": {
+    "teaching": [
+      {
+        "code": "COSC2413",
+        "name": "Web Programming"
+      },
+      {
+        "code": "COSC2473",
+        "name": "Introduction to Computer Systems and Platform Technologies" 
+      },
+      {
+        "code": "COSC2737",
+        "name": "IT Infrastructure and Security"
+      }
+    ]
+  }
+}
+
+console.log(tutor.name)
+console.log(tutor.courses)
+console.log(tutor.courses.teaching);
+console.log(tutor.courses.teaching[0].name);
+
+
 
 // Regular expressions
+var regex = /[A-Za-z]{6,}/g;
+var data = "ervin"
 
-// Web storage
+if (regex.test(data)) {
+  return true;
+} else {
+  return false;
+}
