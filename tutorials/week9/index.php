052941d1c16ebd79ec588175b0741334a775740d
Ervin Chua <ervinchua@gmail.com>
2018-05-04 23:13:20 +1000

    Use require function

--- tutorials/week9/index.php
+++ tutorials/week9/index.php
@@ -2,7 +2,7 @@
 
 <!DOCTYPE html>
 <html lang="en">
-    <?php include 'includes/head.php'; ?>
+    <?php require 'includes/head.php'; ?>
     <body>
         <?php include 'includes/header.php'; ?>
 
