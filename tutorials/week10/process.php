238abff8dd9107f127326a8c75c8fc182fcb93a3
Ervin Chua <ervinchua@gmail.com>
2018-05-12 19:58:31 +1000

    Update README with instructions on starting PHP server

--- tutorials/week10/process.php
+++ tutorials/week10/process.php
@@ -4,17 +4,21 @@
   $coursecode = $_POST['coursecode'];
   $price = $_POST['price'];
 
+  $caravan_price = 22;
+  $powered_caravan = 38;
+
   echo "The value of the name variable: " . $name . "\n";
   echo "The value of the coursename variable: " . $coursename . "\n";
   echo "The value of the coursecode variable: " . $coursecode . "\n\n";
 
   echo $name . " is my " . $coursename . " ($coursecode) " . "tutor.\n\n";
 
+  echo "Price excl. GST: $" . number_format($price, 2) . "\n";
   echo "Price incl. GST: ";
   calculateTax($price);
 
 
-
+  
   function calculateTax($price) {
     $total = $price * 1.1;
     echo "$" . number_format($total, 2);
--- tutorials/week9/README.md
+++ tutorials/week9/README.md
@@ -5,3 +5,9 @@
 Please refer to `index.php` for an example on how to use include and require.
 
 The `includes` directory holds individual partial files which are referenced in `index.php` with the `include` function.
+
+### Starting your local PHP web server
+
+Make sure you're in your Assignment 3 directory.
+Run this command in your terminal
+`php -S localhost:4560`
